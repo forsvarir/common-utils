acf9ddcc953a4f87d8b06cde5ea4be20a9210747
Mark Loveland-Armour <forsvarir@blueyonder.co.uk>
2021-03-02 15:19:10 +0000

    Multiple files should be returned if possible

--- src/test/java/common/utils/file/PathIterableTest.java
+++ src/test/java/common/utils/file/PathIterableTest.java
@@ -2,6 +2,7 @@ package common.utils.file;
 
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -17,7 +18,7 @@ class PathIterableTest {
 
     @Test
     void emptyFolder_nothingToIterate() throws IOException {
-        final Path rootFolder = dummyFileSystem.getPath("/emptyFolder");
+        final Path rootFolder = asPath("/emptyFolder");
         Files.createDirectory(rootFolder);
 
         var iterable = new PathIterable(rootFolder);
@@ -28,9 +29,9 @@ class PathIterableTest {
 
     @Test
     void resumeFromLastFile_nothingToIterate() throws IOException {
-        final Path rootFolder = dummyFileSystem.getPath("/folder");
+        final Path rootFolder = asPath("/folder");
         Files.createDirectory(rootFolder);
-        final Path lastFile = dummyFileSystem.getPath("/folder/file.txt");
+        final Path lastFile = asPath("/folder/file.txt");
         Files.createFile(lastFile);
 
         var iterable = new PathIterable(rootFolder, lastFile);
@@ -41,9 +42,9 @@ class PathIterableTest {
 
     @Test
     void folderWithSingleFile_returnsSingleFile() throws IOException {
-        final Path rootFolder = dummyFileSystem.getPath("/folder");
+        final Path rootFolder = asPath("/folder");
         Files.createDirectory(rootFolder);
-        final Path lastFile = dummyFileSystem.getPath("/folder/file.txt");
+        final Path lastFile = asPath("/folder/file.txt");
         Files.createFile(lastFile);
 
         var iterable = new PathIterable(rootFolder);
@@ -54,9 +55,9 @@ class PathIterableTest {
 
     @Test
     void folderWithSingleFile_resumeFromNull_returnsSingleFile() throws IOException {
-        final Path rootFolder = dummyFileSystem.getPath("/folder");
+        final Path rootFolder = asPath("/folder");
         Files.createDirectory(rootFolder);
-        final Path lastFile = dummyFileSystem.getPath("/folder/file.txt");
+        final Path lastFile = asPath("/folder/file.txt");
         Files.createFile(lastFile);
 
         var iterable = new PathIterable(rootFolder, null);
@@ -67,14 +68,40 @@ class PathIterableTest {
 
     @Test
     void folderWithSingleFile_resumeFromRoot_returnsSingleFile() throws IOException {
-        final Path rootFolder = dummyFileSystem.getPath("/folder");
+        final Path rootFolder = asPath("/folder");
         Files.createDirectory(rootFolder);
-        final Path lastFile = dummyFileSystem.getPath("/folder/file.txt");
+        final Path lastFile = asPath("/folder/file.txt");
         Files.createFile(lastFile);
 
         var iterable = new PathIterable(rootFolder, rootFolder);
         var filesToIterate = StreamSupport.stream(iterable.spliterator(), false);
 
         assertThat(filesToIterate.map(Path::toString)).containsExactly("/folder/file.txt");
+    }
+
+    @Test
+    void folderWithMultipleFiles_returnsFilesInExpectedOrder() throws IOException {
+        final Path rootFolder = asPath("/folder");
+        Files.createDirectory(rootFolder);
+        final Path file1 = asPath("/folder/file1.txt");
+        final Path file2 = asPath("/folder/file2.txt");
+        final Path file3 = asPath("/folder/file3.txt");
+        Files.createFile(file1);
+        Files.createFile(file3);
+        Files.createFile(file2);
+
+        var iterable = new PathIterable(rootFolder, rootFolder);
+        var filesToIterate = StreamSupport.stream(iterable.spliterator(), false);
+
+        assertThat(filesToIterate.map(Path::toString)).containsExactly(
+                "/folder/file1.txt",
+                "/folder/file2.txt",
+                "/folder/file3.txt"
+        );
+    }
+
+    @NotNull
+    private Path asPath(String path) {
+        return dummyFileSystem.getPath(path);
     }
 }
